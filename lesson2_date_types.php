00891846b4b25e1091cabdc8b11b1464ca629e0b
harut666 <38761758+harut666@users.noreply.github.com>
2021-11-21 17:59:50 +0400

    data_types
    boolean true / false
    integer 45
    float   45.5
    string  'Ripsime'

---
+++ lesson2_date_types.php
@@ -0,0 +1,27 @@
+<?php
+/*
+boolean true / false
+integer 45
+float   45.5
+string  'Ripsime'
+*/
+
+//when var equal false we see empty line
+$var = true;
+echo $var . "\n\t";
+
+$var1 = false;
+var_dump($var1);
+
+//integer
+$int = 123;
+echo $int;
+//float
+$fl = 1.2;
+var_dump($fl);
+//string
+$str = 'This is \'string\' $fl';
+$str2 = "This is string  $fl";
+
+
+
